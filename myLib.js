6b22db7d4fee5bf5636a037778befefb8ed8fec7
sajrus33 <brianwala22@gmail.com>
2019-02-10 12:58:10 +0100

    ..
    .

--- myLib.js
+++ myLib.js
@@ -1,24 +1,25 @@
 const MyLib = {
-    howMuchTimeLeft: function (arg, since, untill) {
-        // return number = how long that's gonna take 
-        const myTime = new Date().getTime();
-        switch (arg) {
-            case "hours":
-                return Math.floor((untill / (1000 * 60 * 60) - since / (1000 * 60 * 60)) % 24);
-                break;
-            case "days":
-                return Math.floor((untill / (1000 * 60 * 60 * 24)) - (since / (1000 * 60 * 60 * 24)));
-                break;
-            case "minutes":
-                return Math.floor((untill / (1000 * 60) - since / (1000 * 60)) % 60);
-                break;
-            case "seconds":
-                return Math.floor((untill / 1000 - since / 1000) % 60);
-                break;
-            default:
-                console.log("First argument value can be only : hours, days, minutes, seconds");
-                return "Wrong argument";
-
-        }
+  howMuchTimeLeft: function(arg, since, untill) {
+    // return number = how long that's gonna take
+    const myTime = new Date().getTime();
+    switch (arg) {
+      case "hours":
+        return Math.floor(
+          (untill / (1000 * 60 * 60) - since / (1000 * 60 * 60)) % 24
+        );
+      case "days":
+        return Math.floor(
+          untill / (1000 * 60 * 60 * 24) - since / (1000 * 60 * 60 * 24)
+        );
+      case "minutes":
+        return Math.floor((untill / (1000 * 60) - since / (1000 * 60)) % 60);
+      case "seconds":
+        return Math.floor((untill / 1000 - since / 1000) % 60);
+      default:
+        console.log(
+          "First argument value can be only : hours, days, minutes, seconds"
+        );
+        return "Wrong argument";
     }
-}
+  }
+};
