188fffe80d8450a44032169e8661d6576ac7a317
sajrus33 <brianwala22@gmail.com>
2019-02-14 04:22:26 +0100

    ..
    ,

--- appToDo/script.js
+++ appToDo/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const taskSubmit = document.querySelector(".editor__submit--task");
 const taskDescribe = document.querySelector(".editor__describe--task");
 
@@ -7,7 +8,7 @@ const createBtn = document.querySelector(".editor__create");
 const createPanel = document.querySelector(".editor__wrapper--category")
 
 const searchInput = document.querySelector(".editor__search");
-const tasks = document.querySelectorAll(".main__task");
+let tasks = document.querySelectorAll(".main__task");
 const list = document.querySelector(".main");
 console.log(tasks);
 
@@ -21,11 +22,19 @@ const searchTask = e => {
   activeTasks = activeTasks.filter(task =>
     task.textContent.toLowerCase().includes(searchText)
   );
-  // remove complete li element
-  tasks.forEach(task => task.parentNode.remove());
-  activeTasks.forEach(task => list.appendChild(task.parentNode));
-};
+  // remove complete li element1
+  tasks.forEach(task => task.parentNode.style.display = "none");
 
+  tasks.forEach((task) => {
+    activeTasks.forEach(active => {
+      if (task == active) {
+        task.parentNode.style.display = "flex";
+      }
+    });
+  });
+  // activeTasks.forEach(task => task.parentNode.style.display = "flex");
+
+}
 const btnsDone = document.querySelectorAll(
   "li.main__li button.main__button--done"
 );
@@ -35,7 +44,43 @@ const getTaskDescribe = e => {
 }
 
 const createTask = (describe) => {
-  list.
+  // <li class="main__li">
+  //   <button class="main__button main__button--done">done</button>
+  //   <button class="main__button main__button--delete">delete</button>
+  //   <p class="main__task">nie panie</p>
+  // </li>
+  //li
+  const newTask = document.createElement("li");
+  newTask.classList.add("main__li");
+  //done
+  const newDone = document.createElement("button");
+  newDone.classList.add("main__button");
+  newDone.classList.add("main__button--done");
+  newDone.innerText = "done";
+
+  //delete
+  const newDelete = document.createElement("button");
+  newDelete.classList.add("main__button");
+  newDelete.classList.add("main__button--delete");
+  newDelete.innerText = "delete";
+  //p
+  const newDescribe = document.createElement("p");
+  newDescribe.classList.add("main__task");
+  newDescribe.innerText = newTaskDescribe;
+
+  //complete 
+  newTask.appendChild(newDone);
+  newTask.appendChild(newDelete);
+
+  newTask.appendChild(newDescribe);
+
+  list.appendChild(newTask);
+
+
+  console.log(newTask, newDone, newDescribe);
+  // newTask.appendChild();
+  // list.appendChild()
+  // list.
 }
 
 const doneTask = e => {
@@ -46,10 +91,12 @@ const doneTask = e => {
   e.target.remove();
 };
 const deleteTask = e => {
-  e.target.parentNode.style.transform = "translate(150%," + 0 + "%)";
-  setTimeout(function () {
-    e.target.parentNode.remove();
-  }, 350);
+  e.target.parentNode.remove();
+  // wyszukiwane=noralne
+  // e.target.parentNode.style.transform = "translate(150%," + 0 + "%)";
+  console.log(e.target.parentNode);
+  // setTimeout(function () {
+  // }, 50);
 };
 
 
@@ -67,8 +114,6 @@ createBtn.addEventListener("click", function () {
   createPanel.classList.toggle("off");
 
 });
-
-
 
 
 taskSubmit.addEventListener("click", createTask(newTaskDescribe), false);
