5ef9bb4612f5d2fb8f306dee58a45e49e9aa9128
sajrus33 <brianwala22@gmail.com>
2019-02-28 14:38:13 +0100

    finish todo
    ..

--- appToDo/script.js
+++ appToDo/script.js
@@ -1,9 +1,12 @@
 "use strict";
 let canLocalStorage = false;
-let taskCharLimit = 50;
-let categoryCharLimit = 15;
+const taskCharLimit = 50;
+const categoryCharLimit = 15;
 let categorySelected = false;
-
+let history = {
+  done: [],
+  deleted: []
+};
 let locals = {
   task: {
     txt: [],
@@ -15,61 +18,78 @@ let locals = {
     bgc: [],
     color: []
   }
-}
+};
+
+// test if localStorage exist
 function testLocalStorage() {
   try {
     localStorage.setItem("foo", "foo");
     localStorage.removeItem("foo");
     return true;
   } catch (e) {
+    alert("We are sorry but your browser don't support localStorage");
+
     return false;
   }
 }
+// init
 function init() {
   if (!testLocalStorage) {
-    alert("We are sorry but you cannot use localStorage");
+    myAlert("We are sorry but you cannot use localStorage");
   } else {
     console.log("great local storage working");
     canLocalStorage = testLocalStorage();
-
-    ////////////////////////////////////////////////////////////////////////////////////////////
-
-    let storagedCategory = JSON.parse(localStorage.getItem("catTxt"));
-    let storagedBgc = JSON.parse(localStorage.getItem("catBgc"));
-    let storagedFontColor = JSON.parse(localStorage.getItem("catColor"));
-
+    const storagedCategory = JSON.parse(localStorage.getItem("catTxt"));
+    const storagedBgc = JSON.parse(localStorage.getItem("catBgc"));
+    const storagedFontColor = JSON.parse(localStorage.getItem("catColor"));
     console.log(storagedCategory);
     if (storagedCategory) {
       storagedCategory.forEach((category, i) => {
-        console.log(category, storagedCategory[i], storagedBgc[i], storagedFontColor[i]);
+        console.log(
+          category,
+          storagedCategory[i],
+          storagedBgc[i],
+          storagedFontColor[i]
+        );
 
-        createCategory(storagedCategory[i], storagedBgc[i], storagedFontColor[i], false);
+        createCategory(
+          storagedCategory[i],
+          storagedBgc[i],
+          storagedFontColor[i],
+          false
+        );
       });
     }
-
-    let storagedTask = JSON.parse(localStorage.getItem("taskTxt"));
-    let storagedTaskBgc = JSON.parse(localStorage.getItem("taskBgc"));
-    let storagedTaskFontColor = JSON.parse(localStorage.getItem("taskColor"));
-    console.log(storagedTask, storagedTaskBgc, storagedTaskFontColor);
+    const storagedTask = JSON.parse(localStorage.getItem("taskTxt"));
+    const storagedTaskBgc = JSON.parse(localStorage.getItem("taskBgc"));
+    const storagedTaskFontColor = JSON.parse(localStorage.getItem("taskColor"));
     if (storagedTask) {
       storagedTask.forEach((task, i) => {
         createTask(task, storagedTaskBgc[i], storagedTaskFontColor[i], false);
       });
     }
-
   }
-  ///////////////////////////////////////////////////////////////////////////////////////////////
+}
+// my own alert
+function myAlert(describe) {
+  const oldAlert = document.querySelector(".tip");
+  if (oldAlert) {
+    oldAlert.remove();
+  }
+  const newAlert = document.createElement("div");
+  newAlert.classList.add("tip");
+  newAlert.innerText = describe;
+  document.body.appendChild(newAlert);
 }
 //categorys list
 const categoryList = document.querySelector(".editor__category");
 const categorysWrappper = document.querySelector(".editor__categorys");
-let categorys = document.querySelectorAll(".editor__category--option");
-let categorysBtn = document.querySelectorAll(".editor__category--button");
+let categorys, categorysBtn;
 
 function reoladCategorys() {
   categorysBtn = document.querySelectorAll(".editor__category--button");
   [...categorysBtn].forEach(category => {
-    category.addEventListener("click", deleteCategory);//delete !!
+    category.addEventListener("click", deleteCategory); //delete !!
   });
 
   categorys = document.querySelectorAll(".editor__category--option");
@@ -89,20 +109,16 @@ const searchInput = document.querySelector(".editor__search");
 
 //list and tasks
 const list = document.querySelector(".main");
-let tasks = document.querySelectorAll(".main__task");
 
-let btnsDelete = document.querySelectorAll(
-  "button.main__button--delete"
-);
+let btnsDone, btnsDelete, tasks;
 
 function reoladList() {
+  console.log(tasks);
   tasks = document.querySelectorAll(".main__task");
-
-  btnsDelete = document.querySelectorAll(
-    "button.main__button--delete"
-  );
-  btnsDelete.forEach(item =>
-    item.addEventListener("click", deleteTask));
+  btnsDone = document.querySelectorAll("button.main__button--done");
+  btnsDone.forEach(item => item.addEventListener("click", doneTask));
+  btnsDelete = document.querySelectorAll("button.main__button--delete");
+  btnsDelete.forEach(item => item.addEventListener("click", deleteTask));
 }
 // new category panel
 const createPanel = document.querySelector(".editor__wrapper--category");
@@ -111,39 +127,15 @@ const createPanel = document.querySelector(".editor__wrapper--category");
 const categoryDescribe = document.querySelector(".editor__describe--category");
 const selectCategoryBgc = document.querySelector(".editor__category--bgc");
 const selectCategoryFont = document.querySelector(".editor__category--font");
-const btnCategorySubmit = document.querySelector(".editor__submit--category")
+const btnCategorySubmit = document.querySelector(".editor__submit--category");
 const btnCategoryBack = document.querySelector(".editor__cancel--category");
 
-function setSelectBgc() {
-  this.style.backgroundColor = this.options.item(this.selectedIndex).text;
-  //style pre brother
-  this.previousElementSibling.style.backgroundColor = this.style.backgroundColor;
-}
-function setSelectFontColor() {
-  this.style.backgroundColor = this.options.item(this.selectedIndex).text;
-  //style pre pre brother
-  this.previousElementSibling.previousElementSibling.style.color = this.style.backgroundColor;
-}
-function setSelectBoth() {
-  this.style.backgroundColor = this.options.item(this.selectedIndex).text;
-  this.style.color = this.style.backgroundColor;
-  this.style.backgroundColor = this.options.item(this.selectedIndex).text;
-  this.style.backgroundColor = this.style.backgroundColor;
-}
-
-
-
-
-
-
-
-
 //                                                   fun
-// 
+//
 function toggleMenu() {
   searchPanel.classList.toggle("displayFlex");
   createPanel.classList.toggle("displayFlex");
-};
+}
 const searchTask = e => {
   const searchText = e.target.value.toLowerCase();
 
@@ -152,52 +144,69 @@ const searchTask = e => {
   activeTasks = activeTasks.filter(task =>
     task.textContent.toLowerCase().includes(searchText)
   );
-  // hide LIST elements
-  tasks.forEach(task => task.parentNode.style.display = "none");
-  // show LIST elements
-  tasks.forEach((task) => {
+  tasks.forEach(task => (task.parentNode.style.display = "none"));
+  tasks.forEach(task => {
     activeTasks.forEach(active => {
       if (task == active) {
         task.parentNode.style.display = "flex";
       }
     });
   });
+};
+// tasks menagment
+// under function
+function setLocalTasks() {
+  localStorage.setItem("taskTxt", JSON.stringify(locals.task.txt));
+  localStorage.setItem("taskBgc", JSON.stringify(locals.task.bgc));
+  localStorage.setItem("taskColor", JSON.stringify(locals.task.color));
 }
-const selectCategory = e => {
-  categorysWrappper.classList.toggle("displayFlex");
-  categoryList.style.backgroundColor = e.target.style.backgroundColor;
-  categoryList.style.color = e.target.style.color;
-  categoryList.innerText = e.target.innerText.replace("delete", "");
-  if (!categorySelected) {
-    categorySelected = true;
-  }
-}
+// create task
 const createTask = (describe, bgc, fontColor, create) => {
   if (create) {
-    if (!categorySelected) return alert("Pick category!");
-    if (!describe) return alert("Task describe is empty");
-    if (describe.length > taskCharLimit) return alert("Task describe is too long, max " + taskCharLimit + " characters");
+    let alreadyExist = false;
+    if (!categorySelected) return myAlert("Pick category!");
+    if (!describe) return myAlert("Task describe is empty");
+    if (describe.length > taskCharLimit)
+      return myAlert(
+        "Task describe is too long, max " + taskCharLimit + " characters"
+      );
+    if (tasks) {
+      tasks.forEach(cat => {
+        if (cat.innerText === describe) {
+          alreadyExist = true;
+        }
+      });
+    }
+    if (alreadyExist) return myAlert(describe + " already exist");
   }
-
   //li
   const newTask = document.createElement("li");
   newTask.classList.add("main__li");
   newTask.style.backgroundColor = bgc;
   newTask.style.color = fontColor;
-
   //delete
-
-  const newDelete = document.createElement("button");
-  newDelete.classList.add("main__button");
-  newDelete.classList.add("main__button--delete");
-  newDelete.innerText = "delete";
+  const newDone = document.createElement("button");
+  newDone.classList.add("main__button");
+  newDone.classList.add("main__button--done");
+  // newDelete.innerText = "delete";
   //p
   const newDescribe = document.createElement("p");
   newDescribe.classList.add("main__task");
   newDescribe.innerText = describe;
-  //complete 
-  newTask.appendChild(newDelete);
+  // edit
+  const newEdit = document.createElement("button");
+  newEdit.classList.add("main__button");
+  newEdit.classList.add("main__button--edit");
+  //delete
+  const newDelete = document.createElement("button");
+  newDelete.classList.add("main__button");
+  newDelete.classList.add("main__button--delete");
+  //complete
+  newTask.appendChild(newDone);
   newTask.appendChild(newDescribe);
+  newTask.appendChild(newEdit);
+  newTask.appendChild(newDelete);
+
   list.appendChild(newTask);
   reoladList();
 
@@ -205,17 +214,38 @@ const createTask = (describe, bgc, fontColor, create) => {
     locals.task.txt.push(describe);
     locals.task.bgc.push(bgc);
     locals.task.color.push(fontColor);
-    localStorage.setItem("taskTxt", JSON.stringify(locals.task.txt));
-    localStorage.setItem("taskBgc", JSON.stringify(locals.task.bgc));
-    localStorage.setItem("taskColor", JSON.stringify(locals.task.color));
+    setLocalTasks();
   }
+};
+// categorys menagment
+// under function
+function setLocalCategory() {
+  localStorage.setItem("catTxt", JSON.stringify(locals.cat.txt));
+  localStorage.setItem("catBgc", JSON.stringify(locals.cat.bgc));
+  localStorage.setItem("catColor", JSON.stringify(locals.cat.color));
 }
-
-// localStorage.clear();
+// create cat
 const createCategory = (describe, bgc, fontColor, create) => {
   if (create) {
-    if (!describe || !bgc || !fontColor) return alert("pick color and name your category");
-    if (describe.length > categoryCharLimit) return alert("Category describe is too long, max " + categoryCharLimit + " characters");
+    let alreadyExist = false;
+    if (!describe || !bgc || !fontColor)
+      return myAlert("pick color and name your category");
+    if (describe.length > categoryCharLimit)
+      return myAlert(
+        "Category describe is too long, max " +
+          categoryCharLimit +
+          " characters"
+      );
+    if (categorys) {
+      categorys.forEach(cat => {
+        if (cat.innerText === describe) {
+          alreadyExist = true;
+        }
+      });
+    }
+    if (alreadyExist) return myAlert(describe + " already exist");
+
+    myAlert("Category: " + describe + " created");
   }
 
   const newCategory = document.createElement("div");
@@ -225,10 +255,7 @@ const createCategory = (describe, bgc, fontColor, create) => {
   newCategory.style.color = fontColor;
   const newButton = document.createElement("button");
   newButton.classList.add("editor__category--button");
-  newButton.innerText = "delete";
   newCategory.appendChild(newButton);
-
-
 
   categorysWrappper.appendChild(newCategory);
 
@@ -238,105 +265,130 @@ const createCategory = (describe, bgc, fontColor, create) => {
     locals.cat.txt.push(describe);
     locals.cat.bgc.push(bgc);
     locals.cat.color.push(fontColor);
-    localStorage.setItem("catTxt", JSON.stringify(locals.cat.txt));
-    localStorage.setItem("catBgc", JSON.stringify(locals.cat.bgc));
-    localStorage.setItem("catColor", JSON.stringify(locals.cat.color));
+    setLocalCategory();
   }
-
-}
-
+};
+// delete cat
 const deleteCategory = e => {
-
   const toDelet = locals.cat.txt.indexOf(e.target.parentNode.innerText);
-
-  console.log(toDelet);
-
   for (let prop in locals.cat) {
     if (locals.cat.hasOwnProperty(prop)) {
-      console.log("here is", prop);
       locals.cat[prop].splice(toDelet, 1);
     }
   }
-
   e.target.parentNode.remove();
   reoladCategorys();
-
   if (canLocalStorage) {
-    localStorage.setItem("catTxt", JSON.stringify(locals.cat.txt));
-    localStorage.setItem("catBgc", JSON.stringify(locals.cat.bgc));
-    localStorage.setItem("catColor", JSON.stringify(locals.cat.color));
+    setLocalCategory();
   }
 };
-
-
-// localStorage.clear();
-
-//                                   LISTEN !
-
-selectCategoryBgc.addEventListener("change", setSelectBgc, true);
-selectCategoryFont.addEventListener("change", setSelectFontColor, true);
-categoryList.addEventListener("change", setSelectBoth);
-
-btnCategorySubmit.addEventListener("click", function () {
-  createCategory(categoryDescribe.value, categoryDescribe.style.backgroundColor, categoryDescribe.style.color, true)
-});
-
-categoryList.addEventListener("click", function () {
-  if (categorys.length) {
-    categorysWrappper.classList.toggle("displayFlex");
-  } else {
-    alert("Lets create new category !");
+// select cat
+const selectCategory = e => {
+  categorysWrappper.classList.toggle("displayFlex");
+  categoryList.style.backgroundColor = e.target.style.backgroundColor;
+  categoryList.style.color = e.target.style.color;
+  categoryList.innerText = e.target.innerText.replace("delete", "");
+  if (!categorySelected) {
+    categorySelected = true;
   }
-});
-
-
-const doneTask = e => {
-  e.target.parentNode.style.transform = "translate( calc(50% - 2px),0)";
-  e.target.parentNode.style.backgroundColor = "black";
-  e.target.remove();
 };
+// under fun for listen
+function setSelectBgc() {
+  this.style.backgroundColor = this.options.item(this.selectedIndex).text;
+  this.previousElementSibling.style.backgroundColor = this.style.backgroundColor;
+}
+function setSelectFontColor() {
+  this.style.backgroundColor = this.options.item(this.selectedIndex).text;
+  this.previousElementSibling.previousElementSibling.style.color = this.style.backgroundColor;
+}
+function setSelectBoth() {
+  this.style.backgroundColor = this.options.item(this.selectedIndex).text;
+  this.style.color = this.style.backgroundColor;
+  this.style.backgroundColor = this.options.item(this.selectedIndex).text;
+  this.style.backgroundColor = this.style.backgroundColor;
+}
 const deleteTask = e => {
-
-  const toDelet = locals.task.txt.indexOf(e.target.nextElementSibling.innerText);
-
-  console.log(locals.task.length);
-
+  e.target.parentNode.style.backgroundColor = "black";
+  e.target.style.display = "none";
+  const toDelet = locals.task.txt.indexOf(e.target.innerText);
+  setTimeout(() => {
+    e.target.parentNode.remove();
+    reoladList();
+  }, 300);
   for (let prop in locals.task) {
     if (locals.task.hasOwnProperty(prop)) {
-      console.log("here is", prop);
       locals.task[prop].splice(toDelet, 1);
     }
   }
-
-  e.target.parentNode.remove();
-  reoladList();
-
   if (canLocalStorage) {
-    localStorage.setItem("taskTxt", JSON.stringify(locals.task.txt));
-    localStorage.setItem("taskBgc", JSON.stringify(locals.task.bgc));
-    localStorage.setItem("taskColor", JSON.stringify(locals.task.color));
+    setLocalTasks();
+  }
+};
+const doneTask = e => {
+  const toDelet = locals.task.txt.indexOf(e.target.innerText);
+  for (let prop in locals.task) {
+    if (locals.task.hasOwnProperty(prop)) {
+      locals.task[prop].splice(toDelet, 1);
+    }
+  }
+  e.target.style.display = "none";
+  e.target.parentNode.style.transform = "translateX(100%)";
+  setTimeout(() => {
+    e.target.parentNode.remove();
+    reoladList();
+  }, 300);
+  if (canLocalStorage) {
+    setLocalTasks();
   }
 };
 
-btnsDelete.forEach(item => item.addEventListener("click", deleteTask));
+//                                   LISTEN !
+
+//creating category
+selectCategoryBgc.addEventListener("change", setSelectBgc, true);
+selectCategoryFont.addEventListener("change", setSelectFontColor, true);
+categoryList.addEventListener("change", setSelectBoth);
+btnCategorySubmit.addEventListener("click", function() {
+  createCategory(
+    categoryDescribe.value,
+    categoryDescribe.style.backgroundColor,
+    categoryDescribe.style.color,
+    true
+  );
+});
+categoryList.addEventListener("click", function() {
+  if (categorys) {
+    categorysWrappper.classList.toggle("displayFlex");
+  } else {
+    myAlert("Lets create new category !");
+  }
+});
 searchInput.addEventListener("input", searchTask);
-
-
+//hheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+searchInput.addEventListener("click", function() {
+  event.target.value = "";
+});
 btnCreateCategory.addEventListener("click", toggleMenu);
 btnCategoryBack.addEventListener("click", toggleMenu);
-
-btnTaskSubmit.addEventListener("click", function () {
-  createTask(taskDescribe.value, categoryList.style.backgroundColor, categoryList.style.color, true);
-}, false);
-
-categoryDescribe.addEventListener("keyup", function (event) {
+btnTaskSubmit.addEventListener(
+  "click",
+  function() {
+    createTask(
+      taskDescribe.value,
+      categoryList.style.backgroundColor,
+      categoryList.style.color,
+      true
+    );
+  },
+  false
+);
+categoryDescribe.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     btnCategorySubmit.click();
   }
 });
-
-taskDescribe.addEventListener("keyup", function (event) {
+taskDescribe.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     btnTaskSubmit.click();
@@ -344,12 +396,6 @@ taskDescribe.addEventListener("keyup", function (event) {
 });
 
 init();
-// on click window menu remove class displayBlock 10 - not into it anymore !
-//add - button arrow up down on taks 
-//add - monsters 8
-//add - remove button for categorys 2
-//style stylee 9 almost
+myAlert("Welcome on ToDo :)");
 
-
-//style: when select category opens->transformr -100 to 0, and bounce soo: -100 10 -10 0 .
 //copyrights by Brian Wala :P fck off cukerberg
