33ca06755786c9a8899217c8fbc222a80c22edd3
sajrus33 <brianwala22@gmail.com>
2019-03-04 15:44:55 +0100

    portfolio ++
    added portofilo folder to nauka-samuraj

---
+++ zuperPortfolioPrace/3/script.js
@@ -0,0 +1,9 @@
+// let animals = ['pierwszy','drugi','trzeci'];
+// animals.splice(0,2);
+// console.log(animals);
+// let tak = true;
+// for(let i = 0; tak; i++){
+var money = '23';
+// }
+alert(`blabla ${money} blabla `)
+
