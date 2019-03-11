7653c91bd0ed94158e902225725b5bca86774f01
sajrus33 <brianwala22@gmail.com>
2019-03-11 03:46:13 +0100

    ..
    ;

--- zuperPortfolioPrace/5/script.js
+++ zuperPortfolioPrace/5/script.js
@@ -1,8 +1,15 @@
 const myDOM = {
+    // header
+    header: document.querySelector(".header"),
+    // menu
     menuLang: document.querySelector(".header__nav--languages"),
     menuLangList: document.querySelector(".languages__list"),
     menuHamb: document.querySelector(".header__nav--hamb"),
     menuHambList: document.querySelector(".hamb__list"),
+    // products
+    productDescribeFirst: document.querySelector(".product__describe--first"),
+
+
 
     listen: function () {
         myDOM.menuLang.addEventListener("click", function () {
@@ -13,8 +20,36 @@ const myDOM = {
             myDOM.menuHambList.classList.toggle("displayNone");
             // }
         });
+        window.addEventListener('scroll', function () {
+            // if(mySize.headerHeight + )
 
-    }
+            // const top = describe.previousElementSibling;
+
+            console.log(mySize.headerHeight, mySize.descrbieFirstTop, window.scrollY);
+            // if (x > top) 
+
+        });
+        window.addEventListener("resize", mySize.resize);
+
+    },
+
 }
 
-myDOM.listen();
+let mySize = {
+    headerHeight: myDOM.header.offsetHeight,
+    descrbieFirstTop: myDOM.productDescribeFirst.offsetTop,
+
+    resize: function () {
+        mySize.headerHeight = myDOM.header.offsetHeight;
+        mySize.descrbieFirstTop = myDOM.productDescribeFirst.offsetTop,
+
+            console.log("resize");
+    }
+
+}
+
+function init() {
+    myDOM.listen();
+    mySize.resize();
+}
+init();
