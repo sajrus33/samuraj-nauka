dc6bcd8cd9840abbcbbd29856442c9d13c0518ec
sajrus33 <brianwala22@gmail.com>
2019-03-31 04:08:47 +0200

    style
    skew sections,
    responsive in extra.css

---
+++ zuperPortfolioPrace/website portfolio/script.js
@@ -0,0 +1,38 @@
+
+const myDOM = {
+    header: document.querySelector(".header"),
+    about: document.querySelector(".about"),
+    skills: document.querySelector(".skills"),
+    progress: document.querySelector(".progress"),
+    projects: document.querySelector(".projects"),
+    footer: document.querySelector(".footer"),
+
+    scrollTo: function (target = myDOM.header, duration) {
+        const target = target;
+        const targetPosition = target.
+
+    }
+
+
+};
+let mySize = {
+
+}
+
+
+
+
+
+
+
+
+window.addEventListener("resize", function () {
+    console.log("header.offSetBottom");
+
+    console.log(myDOM.about.offsetTop);
+    console.log(myDOM.header.offsetHeight);
+
+});
+
+
+
