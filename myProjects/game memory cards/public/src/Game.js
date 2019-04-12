668095d4e3b40d8b7ef928805aa4a4177fbc0d51
sajrus33 <brianwala22@gmail.com>
2019-04-13 01:18:49 +0200

    //

---
+++ myProjects/game memory cards/public/src/Game.js
@@ -0,0 +1,10 @@
+import * as utilities from "/public/src/utilities.js";
+import * as resources from "/public/src/resources.js";
+
+import { Card } from "/public/src/Card.js";
+import { myAlert } from "/public/src/myAlert.js";
+
+
+console.log({ utilities, myAlert, Card, resources });
+// myAlert();
+
